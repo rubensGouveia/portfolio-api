d11d62b8a2670b1d233a35d687ccca51fbc6234b
Rubens Gouveia <rubensgui.gouveia@gmail.com>
2021-10-30 16:10:30 -0300

    starting api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '445704c344326302b3b6698f1a059883'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '220bf7a4-981e-43bc-b3c3-cbcf55748a48'
+};
